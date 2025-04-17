441236f3b77eaf1c239e8d887ade96754da5c001
Lucas <lucas.baptista@live.com>
2025-04-17 20:28:19 -0300

    código Scaffold alterado salvo em DQL

--- Banco de dados/DQL.sql
+++ Banco de dados/DQL.sql
@@ -1,13 +1,16 @@
---	DQL Data Query Language
+----	DQL Data Query Language
 
--- SELECT
+---- SELECT
 
-select * from Cliente
+--select * from Cliente
 
-select * from Pedido
+--select * from Pedido
 
-select * from Pagamento
+--select * from Pagamento
 
-select * from Produto
+--select * from Produto
 
-select * from ItemPedido
+--select * from ItemPedido
+
+
+--dotnet ef dbcontext scaffold "Data Source=DESKTOP-8ED5M2P\SQLEXPRESS;Initial Catalog=ECommerce;User Id=sa;Password=Senai@134;TrustServerCertificate=true;" Microsoft.EntityFrameworkCore.SqlServer --context-dir Context --output-dir Models --force
